8d11b1d24e4006d08134fdfd4d69a74e8d1e1a6e
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-03-03 22:03:25 +0530

    Add authentication DTOs
    Introduce AuthenticationRequest and AuthenticationResponse DTOs with Lombok annotations. AuthenticationRequest carries username and password for login requests; AuthenticationResponse returns token and role for authenticated responses (includes @Data, @Builder, @AllArgsConstructor, @NoArgsConstructor).

---
+++ order-management-system/src/main/java/savindu_rashmika/order/management/system/dto/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package savindu_rashmika.order.management.system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+    private String role;
+}
