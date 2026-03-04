e71675a2d2f362cc2fcf9c527ea5b98cc85a5e0b
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-03-04 10:25:30 +0530

    Hash passwords; add create/update in UserService
    Add PasswordEncoder to UserService and implement createUser and updateUser methods. createUser encodes the password, sets activeStatus to true and saves the user. updateUser updates name/username/role and only re-encodes the password when a non-empty new password is provided. These changes ensure passwords are stored hashed and provide basic create/update user operations.

--- order-management-system/src/main/java/savindu_rashmika/order/management/system/services/UserService.java
+++ order-management-system/src/main/java/savindu_rashmika/order/management/system/services/UserService.java
@@ -1,6 +1,7 @@
 package savindu_rashmika.order.management.system.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import savindu_rashmika.order.management.system.entities.User;
 import savindu_rashmika.order.management.system.repositories.UserRepository;
@@ -12,6 +13,7 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     public List<User> findAll() {
         return repository.findAll();
@@ -19,6 +21,23 @@ public class UserService {
 
     public void deleteById(Integer id) {
         repository.deleteById(id);
+    }
+
+    public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setActiveStatus(true);
+        return repository.save(user);
+    }
+
+    public User updateUser(Integer id, User userDetails) {
+        User user = repository.findById(id).orElseThrow();
+        user.setName(userDetails.getName());
+        user.setUsername(userDetails.getUsername());
+        user.setRole(userDetails.getRole());
+        if (userDetails.getPassword() != null && !userDetails.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(userDetails.getPassword()));
+        }
+        return repository.save(user);
     }
 
     public User updateStatus(Integer id, boolean status) {
