5a3bc420da28f0a00cbbf2e8628d0017afc263e7
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-03-04 10:30:03 +0530

    Add active/inactive customer queries
    Add List import and two repository methods to CustomerRepository: findByIsActiveTrue() and findByIsActiveFalse(). These provide simple derived-query methods to fetch active or inactive customers by the isActive flag without custom JPQL.

--- order-management-system/src/main/java/savindu_rashmika/order/management/system/repositories/CustomerRepository.java
+++ order-management-system/src/main/java/savindu_rashmika/order/management/system/repositories/CustomerRepository.java
@@ -3,5 +3,10 @@ package savindu_rashmika.order.management.system.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import savindu_rashmika.order.management.system.entities.Customer;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    List<Customer> findByIsActiveTrue();
+
+    List<Customer> findByIsActiveFalse();
 }
