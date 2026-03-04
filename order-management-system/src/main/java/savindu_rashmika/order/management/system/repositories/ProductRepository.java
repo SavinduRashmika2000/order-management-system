52f679f3da4c8918a40eeec1b93dbb8f9ac3412a
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-03-04 19:38:24 +0530

    Add findByStatus to ProductRepository
    Expose a derived query method findByStatus(String) in ProductRepository to retrieve a list of products filtered by their status using Spring Data JPA. Also added the required java.util.List import.

--- order-management-system/src/main/java/savindu_rashmika/order/management/system/repositories/ProductRepository.java
+++ order-management-system/src/main/java/savindu_rashmika/order/management/system/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package savindu_rashmika.order.management.system.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import savindu_rashmika.order.management.system.entities.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByStatus(String status);
 }
